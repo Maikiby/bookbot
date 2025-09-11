014de5de01ed770c91eebb70d8e9acd5d6f6ed1c
Maikiby <maikiby@pm.me>
2025-09-11 00:23:05 -0500

    added word counter function

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
+
+def count_words(book_text):
+    num_words = 0
+    word_list = book_text.split()
+    for word in word_list:
+        num_words += 1
+    return num_words
+
+def main():
+    word_count = count_words(get_book_text("./books/frankenstein.txt"))  
+    message = f"{word_count} words found in the document"
+    print(message)
+
+main()
